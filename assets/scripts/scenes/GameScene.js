70481b77616ecd8ec29ea46272645611e7799178
Ren Tamdang <tamdangren@gmail.com>
2025-05-10 15:01:33 +0800

    Update GameScene.js

--- assets/scripts/scenes/GameScene.js
+++ assets/scripts/scenes/GameScene.js
@@ -1,5 +1,8 @@
 let player;
 let cursors;
+let spacebar;
+let playerProjectiles;
+let meteors;
 
 class GameScene extends Phaser.Scene {
     constructor() {
@@ -11,12 +14,18 @@ class GameScene extends Phaser.Scene {
         this.load.spritesheet('player', '/assets/images/entities/SpaceShooterAssetPack_Ships.png',
             { frameWidth: 8, frameHeight: 8 }
         );
+        this.load.spritesheet('projectile', '/assets/images/projectile/SpaceShooterAssetPack_Projectiles.png',
+            { frameWidth: 8, frameHeight: 8 }
+        )
+        this.load.spritesheet('misc', '/assets/images/misc/SpaceShooterAssetPack_Miscellaneous.png',
+            { frameWidth: 8, frameHeight: 8 }
+        )
     }
 
     create() {
         this.add.image(0, 0, 'bg').setOrigin(0, 0);
 
-        player = this.physics.add.sprite(200, 850, 'player').setFrame(1).setScale(3);
+        player = this.physics.add.sprite(200, 850, 'player').setFrame(1).setScale(4);
 
         // creating animations for player
         this.anims.create({
@@ -48,10 +57,25 @@ class GameScene extends Phaser.Scene {
         })
 
         cursors = this.input.keyboard.createCursorKeys();
+        spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        // creating player projectiles
+        playerProjectiles = this.physics.add.group(); 
+
+        // creating meteors
+        meteors = this.physics.add.group();
+
+        spacebar.on('down', () => this.shootProjectile(player));
+
+        let timer = this.time.addEvent({
+            delay: 1000,
+            callback: this.spawnMeteor,
+            callbackScope: this,
+            loop: true
+        })
     }
 
     update() {
-        // movement
         if (cursors.left.isDown) {
             player.setVelocityX(-180);
             player.anims.play('left',true);
@@ -64,5 +88,18 @@ class GameScene extends Phaser.Scene {
             player.setVelocityX(0);
             player.anims.play('center');
         }
+    }
+
+    shootProjectile(player) {
+        let playerProjectile = playerProjectiles.create(player.x, player.y, 'projectile').setFrame(7).setScale(4);
+        playerProjectile.setVelocity(0,-player.y);
+        playerProjectile.allowGravity = false;
+    }
+
+    spawnMeteor() {
+        let meteor = meteors.create(Phaser.Math.Between(0,400),-200, 'misc').setFrame(40).setScale(4);
+        meteor.setVelocity(0,200);
+        meteor.allowGravity = false;
+        console.log("meteor created");
     }
 }
